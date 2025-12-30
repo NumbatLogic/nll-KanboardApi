aee940235ffae99422052c50628c92e5c699e20f
CliffyA <github@cliffya.com>
2025-12-30 14:59:14 +1100

    Moving over codes from KanboardServer

---
+++ KanboardApi.php
@@ -0,0 +1,14 @@
+<?php
+	class KanboardApi_Config extends Project_Config
+	{	
+		public function __construct($sAction)
+		{
+			parent::__construct($sAction);
+			$this->m_xFileArray = ProjectGen_ParseDirectory(dirname(__FILE__), "/\.h$|\.c$|\.hpp$|\.cpp$/");
+		}
+
+		public function GetName() { return "KanboardApi"; }
+		public function GetKind() { return KIND_STATIC_LIBRARY; }
+		public function GetBaseDirectory() { return dirname(__FILE__); }
+	}
+?>
